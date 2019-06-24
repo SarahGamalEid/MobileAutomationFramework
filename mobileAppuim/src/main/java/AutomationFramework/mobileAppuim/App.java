a8a5519b39ee25cef091212026897c3fff1c3048
SarahGamal <SarahGamal@DESKTOP-OPQU85J.cicsv.eg.ibm.com>
2019-06-24 15:32:32 +0200

    1st

---
+++ mobileAppuim/src/main/java/AutomationFramework/mobileAppuim/App.java
@@ -0,0 +1,13 @@
+package AutomationFramework.mobileAppuim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
